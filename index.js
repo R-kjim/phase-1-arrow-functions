341d71fbfdca07e6dffd11cd7fd19eff9026192f
R-kjim <rkayjim@gmail.com>
2024-06-26 07:05:45 +0300

    creates arrow functions

---
+++ index.js
@@ -0,0 +1,9 @@
+//create arrow functions
+//an arrow function that returns the results of division
+const divide = (x,y) => x/y
+
+//an arrow function that takes in a number and returns its square
+const square=(x) => x**2
+
+//an arrow function that takes in two parameters and adds them together
+const add=(x,y) => x+y;
